8b1cd7ec27b01cb6be6af3018a1a33f332ea46a8
João Victor Pinheiro <57499717+Jotave99@users.noreply.github.com>
2023-11-24 10:40:54 -0300

    API gateway e autenticacao feita por Robson

---
+++ API-gateway/app.js
@@ -0,0 +1,70 @@
+import express from "express";
+import nunjucks from "nunjucks";
+import * as middleware from "./src/middleware/authorization.js";
+import cookieParser from 'cookie-parser';
+import bodyParser from "body-parser";
+import session from 'express-session';
+import { config } from "dotenv";
+import fs  from "fs";
+config();
+
+const app = express();
+
+//app.use(middleware.locals);
+app.use(express.static('public'));
+app.set('view engine', 'njk');
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 7 * 24 * 60 * 60 * 1000}
+}))
+
+nunjucks.configure('src/views', {
+    autoescape: true,
+    express: app
+});
+
+app.get("/", (req, res) => {
+    res.render("landingpage.njk");
+})
+
+app.get("/search", middleware.authenticate, (req, res) => {
+
+    const telaConfigData = fs.readFileSync("./telaConfig.json", "utf8");
+
+    const telaConfig = JSON.parse(telaConfigData);
+
+    res.render("search.njk", {telaConfig});
+});
+
+let comments = [];
+
+app.get('/api/comments', (req, res) => {
+  res.json(comments);
+});
+
+app.post('/api/comments', (req, res) => {
+  const { author, text } = req.body;
+
+  if (author && text) {
+    const newComment = { author, text };
+    comments.push(newComment);
+
+    res.redirect('/comments');
+  } else {
+    res.status(400).json({ error: 'Author and text are required.' });
+  }
+});
+
+app.get('/comments', (req, res) => {
+  res.render('comentarios.njk', { comments });
+});
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
